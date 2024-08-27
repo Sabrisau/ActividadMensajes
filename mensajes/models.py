5e78e4206c66f5c99627225ae16fcfb5b045bba1
Sabrisau <sasaundersvillegas@udc.edu.ar>
2024-08-27 19:54:33 -0300

    Defino el modelo

--- mensajes/models.py
+++ mensajes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Mensaje(models.Model):
+    texto = models.TextField()
+    remitente = models.CharField(max_length=200)
+    destinatario = models.CharField(max_length=200)
+    fecha_envio = models.DateTimeField()
+
+    def __str__(self):
+        return f'De {self.remitente} a {self.destinatario} - {self.texto[:20]}'
