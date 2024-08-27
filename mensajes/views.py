17418d3174b357555857daf1ffc06d60a3826343
Sabrisau <sasaundersvillegas@udc.edu.ar>
2024-08-27 19:46:07 -0300

    Defino views.py

--- mensajes/views.py
+++ mensajes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Mensaje
 
-# Create your views here.
+def mensajes_recibidos(request):
+    # Obtener el destinatario desde la solicitud 
+    destinatario = request.user  # Suponiendo que el usuario actual es el destinatario
+
+    # Filtra los mensajes recibidos por el destinatario
+    mensajes = Mensaje.objects.filter(destinatario=destinatario)
+
+    # Pasa los mensajes a la plantilla
+    return render(request, 'mensajes/recibidos.html', {'mensajes': mensajes})
