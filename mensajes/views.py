49b91d864f0dea02a31481907a3baef16da147f7
Sabrisau <sasaundersvillegas@udc.edu.ar>
2024-08-28 18:27:56 -0300

    Actualización de views y HTML

--- mensajes/views.py
+++ mensajes/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from .models import Mensaje
 
 def mensajes_recibidos(request):
-    # Obtener el destinatario desde la solicitud 
-    destinatario = "Sabrina"
 
     # Filtra los mensajes recibidos por el destinatario
-    mensajes = Mensaje.objects.filter(destinatario=destinatario)
+    mensajes = Mensaje.objects.filter(destinatario='Sabrina')
 
     # Pasa los mensajes a la plantilla
     return render(request, 'recibidos.html', {'mensajes': mensajes})
